9ed52d490d0c934d71071f73b6eaf6f6b26eff8f
Angelina <angelinaivanova16@gmail.com>
2023-06-24 21:27:29 +0300

    add layout to dialogs

--- src/components/dialogs/Dialogs.jsx
+++ src/components/dialogs/Dialogs.jsx
@@ -1,9 +1,21 @@
 import classes from './dialogs.module.css'
+import { NavLink } from 'react-router-dom';
 
 const Dialogs = () => {
   return (
-    <div>
-      dialogs
+    <div className={ classes.wrapper }>
+      <ul className={ classes.dialogitems }>
+        <li className={ classes.dialog }><NavLink to='/sveta' className={ ({ isActive }) => isActive ? classes.active : classes.link }>Sveta</NavLink></li>
+        <li className={ classes.dialog }><NavLink to='/kolya' className={ ({ isActive }) => isActive ? classes.active : classes.link }>Kolya</NavLink></li>
+        <li className={ classes.dialog }><NavLink to='/victor' className={ ({ isActive }) => isActive ? classes.active : classes.link }>Victor</NavLink></li>
+        <li className={ classes.dialog }><NavLink to='/masha' className={ ({ isActive }) => isActive ? classes.active : classes.link }>Masha</NavLink></li>
+        <li className={ classes.dialog }><NavLink to='/sam' className={ ({ isActive }) => isActive ? classes.active : classes.link }>Sam</NavLink></li>
+      </ul>
+      <div className='{ classes.message-items }'>
+        <div>Hi</div>
+        <div>I`m so happy for you!</div>
+        <div>Today is gonna be a good day.</div>
+      </div>
     </div>
   )
 }
