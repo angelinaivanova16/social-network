daed86791dacf44b7c282d82924b3b6021275489
Angelina <angelinaivanova16@gmail.com>
2023-10-16 14:00:11 +0300

    create class component Users and add get request there to get users-data

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,13 +1,62 @@
+import React from "react";
 import classes from "./users.module.css";
 import axios from "axios";
 
-const Users = (props) => {
-
-  if(props.users.length === 0) {
-    axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
+      axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
       props.setUsers(response.data.items)
-    })
+      })
   }
+  render() {
+    return (
+      <div className={classes.wrapper}>
+        <h1 className={classes.usersTitle}>Users</h1>
+        <div className="users">
+          <ul>
+            {/* промапим каждого пользователя и создадим li на каждого */}
+            {this.props.users.map((el) => (
+              <li key={el.id} className={classes.user}>
+                <div className={classes.userAvatar}>                <img className={classes.ava} src={el.photos.small != null ? el.photos.small : "/images/userIcon.jpg"} alt="ava" />
+                  {el.followed ? (
+                    <button
+                      onClick={() => {
+                        this.props.follow(el.id);
+                      }}
+                      className={classes.userBtn}
+                    >
+                      Unfollow
+                    </button>
+                  ) : (
+                    <button
+                      onClick={() => {
+                        this.props.unFollow(el.id);
+                      }}
+                      className={classes.userBtn}
+                    >
+                      Follow
+                    </button>
+                  )}
+                </div>
+                <div className={classes.userInfo}>
+                  <div className={classes.userNameAndStatus}>
+                    <p className={classes.userName}>{el.name}</p>
+                    <p className={classes.userStatus}>{el.status}</p>
+                  </div>
+                  <div className={classes.userLocation}>
+                    <p className={classes.city}>{'el.location.city'}</p>
+                    <p className={classes.country}>{'el.location.country'}</p>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
 
   // if(props.users.length === 0) {
   //   props.setUsers([
@@ -45,53 +94,5 @@ const Users = (props) => {
   //       },
   //     ])
   // }
-
-  return (
-    <div className={classes.wrapper}>
-      <h1 className={classes.usersTitle}>Users</h1>
-      <div className="users">
-        <ul>
-          {/* промапим каждого пользователя и создадим li на каждого */}
-          {props.users.map((el) => (
-            <li key={el.id} className={classes.user}>
-              <div className={classes.userAvatar}>
-                <img className={classes.ava} src={el.photos.small != null ? el.photos.small : "/images/userIcon.jpg"} alt="ava" />
-                {el.followed ? (
-                  <button
-                    onClick={() => {
-                      props.follow(el.id);
-                    }}
-                    className={classes.userBtn}
-                  >
-                    Unfollow
-                  </button>
-                ) : (
-                  <button
-                    onClick={() => {
-                      props.unFollow(el.id);
-                    }}
-                    className={classes.userBtn}
-                  >
-                    Follow
-                  </button>
-                )}
-              </div>
-              <div className={classes.userInfo}>
-                <div className={classes.userNameAndStatus}>
-                  <p className={classes.userName}>{el.name}</p>
-                  <p className={classes.userStatus}>{el.status}</p>
-                </div>
-                <div className={classes.userLocation}>
-                  <p className={classes.city}>{'el.location.city'}</p>
-                  <p className={classes.country}>{'el.location.country'}</p>
-                </div>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
-};
 
 export default Users;
